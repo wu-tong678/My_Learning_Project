0c7209cd20383b02183484bfa8061a86d91abf51
phoenix tree <1705419529@qq.com>
2026-06-01 12:59:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import ZhipuAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.chat_models import ChatZhipuAI
